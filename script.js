ffbfbdbf80fae4df8dc196a5f21c58096602eeaa
santohack <santohack238@gmail.com>
2024-02-06 03:11:17 +0530

    all completed

---
+++ script.js
@@ -0,0 +1,34 @@
+
+function redirectToBirthdayPage() {
+  window.location.href = "birthday.html";
+}
+
+function redirectToIndexPage() {
+  window.location.href = "index.html";
+}
+
+function redirectToBirthtimePage() {
+  window.location.href = "birthtime.html";
+}
+function redirectTobornPage() {
+  window.location.href = "born.html";
+}
+function redirectTo5thpagePage() {
+  window.location.href = "5thpage.html";
+}
+function redirectTo6thpagePage() {
+  window.location.href = "6thpage.html";
+}
+
+function redirectTo7thpagePage() {
+  window.location.href = "7thpage.html";
+}
+function redirectTo8thpagePage() {
+  window.location.href = "8thpage.html";
+}
+function redirectTo9thpagePage() {
+  window.location.href = "9thpage.html";
+}
+function redirectTo10thpagePage() {
+  window.location.href = "10thpage.html";
+}
